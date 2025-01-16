4afd7d843af0505054dccbea2d1ca79e9bd299d2
David Haig <david@ninjametal.com>
2025-01-16 01:35:04 +0000

    Expose i2c error type if encountered

--- src/lib.rs
+++ src/lib.rs
@@ -31,12 +31,12 @@ pub struct Point {
 /// Gt911 Error
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug, Clone)]
-pub enum Error {
+pub enum Error<E> {
     /// Usually indicates that you are attempting to communicate with a device that is not a 911
     /// or that there is a general communication failure
     UnexpectedProductId,
     /// I2C communication error
-    I2C,
+    I2C(E),
     /// Not an actual error, it just means "no new data available"
     /// This means that you have polled the device again in-between it detecting any new touch data
     /// This can safely be ignored
@@ -74,7 +74,7 @@ where
 
     /// Checks the ProductId for a "911\0" string response and resets the status register
     /// Only needs to be called once on startup
-    pub fn init(&self, i2c: &mut I2C) -> Result<(), Error> {
+    pub fn init(&self, i2c: &mut I2C) -> Result<(), Error<E>> {
         // switch to command mode
         self.write(i2c, GT911_COMMAND_REG, 0)?;
 
@@ -99,7 +99,7 @@ where
 
     /// Gets a single touch point
     /// Returns Ok(None) for release, Some(point) for press or move and Err(Error::NotReady) for no data
-    pub fn get_touch(&self, i2c: &mut I2C) -> Result<Option<Point>, Error> {
+    pub fn get_touch(&self, i2c: &mut I2C) -> Result<Option<Point>, Error<E>> {
         let num_touch_points = self.get_num_touch_points(i2c)?;
 
         let point = if num_touch_points > 0 {
@@ -121,7 +121,7 @@ where
     pub fn get_multi_touch(
         &self,
         i2c: &mut I2C,
-    ) -> Result<heapless::Vec<Point, MAX_NUM_TOUCHPOINTS>, Error> {
+    ) -> Result<heapless::Vec<Point, MAX_NUM_TOUCHPOINTS>, Error<E>> {
         let num_touch_points = self.get_num_touch_points(i2c)?;
 
         let points = if num_touch_points > 0 {
@@ -152,7 +152,7 @@ where
         Ok(points)
     }
 
-    fn get_num_touch_points(&self, i2c: &mut I2C) -> Result<usize, Error> {
+    fn get_num_touch_points(&self, i2c: &mut I2C) -> Result<usize, Error<E>> {
         // read coords
         let mut read = [0u8; 1];
         self.read(i2c, GT911_TOUCHPOINT_STATUS_REG, &mut read)?;
@@ -168,15 +168,15 @@ where
         }
     }
 
-    fn write(&self, i2c: &mut I2C, register: u16, value: u8) -> Result<(), Error> {
+    fn write(&self, i2c: &mut I2C, register: u16, value: u8) -> Result<(), Error<E>> {
         let register = register.to_be_bytes();
         let cmd = [register[0], register[1], value];
-        i2c.write(self.i2c_addr, &cmd).map_err(|_e| Error::I2C)
+        i2c.write(self.i2c_addr, &cmd).map_err(Error::I2C)
     }
 
-    fn read(&self, i2c: &mut I2C, register: u16, buf: &mut [u8]) -> Result<(), Error> {
+    fn read(&self, i2c: &mut I2C, register: u16, buf: &mut [u8]) -> Result<(), Error<E>> {
         i2c.write_read(self.i2c_addr, &register.to_be_bytes(), buf)
-            .map_err(|_e| Error::I2C)
+            .map_err(Error::I2C)
     }
 }
 
@@ -211,7 +211,7 @@ where
 
     /// Checks the ProductId for a "911\0" string response and resets the status register
     /// Only needs to be called once on startup
-    pub async fn init(&self, i2c: &mut I2C) -> Result<(), Error> {
+    pub async fn init(&self, i2c: &mut I2C) -> Result<(), Error<E>> {
         // switch to command mode
         self.write(i2c, GT911_COMMAND_REG, 0).await?;
 
@@ -236,7 +236,7 @@ where
 
     /// Gets a single touch point
     /// Returns Ok(None) for release, Some(point) for press or move and Err(Error::NotReady) for no data
-    pub async fn get_touch(&self, i2c: &mut I2C) -> Result<Option<Point>, Error> {
+    pub async fn get_touch(&self, i2c: &mut I2C) -> Result<Option<Point>, Error<E>> {
         let num_touch_points = self.get_num_touch_points(i2c).await?;
 
         let point = if num_touch_points > 0 {
@@ -258,7 +258,7 @@ where
     pub async fn get_multi_touch(
         &self,
         i2c: &mut I2C,
-    ) -> Result<heapless::Vec<Point, MAX_NUM_TOUCHPOINTS>, Error> {
+    ) -> Result<heapless::Vec<Point, MAX_NUM_TOUCHPOINTS>, Error<E>> {
         let num_touch_points = self.get_num_touch_points(i2c).await?;
 
         let points = if num_touch_points > 0 {
@@ -290,7 +290,7 @@ where
         Ok(points)
     }
 
-    async fn get_num_touch_points(&self, i2c: &mut I2C) -> Result<usize, Error> {
+    async fn get_num_touch_points(&self, i2c: &mut I2C) -> Result<usize, Error<E>> {
         // read coords
         let mut read = [0u8; 1];
         self.read(i2c, GT911_TOUCHPOINT_STATUS_REG, &mut read)
@@ -307,18 +307,16 @@ where
         }
     }
 
-    async fn write(&self, i2c: &mut I2C, register: u16, value: u8) -> Result<(), Error> {
+    async fn write(&self, i2c: &mut I2C, register: u16, value: u8) -> Result<(), Error<E>> {
         let register = register.to_be_bytes();
         let cmd = [register[0], register[1], value];
-        i2c.write(self.i2c_addr, &cmd)
-            .await
-            .map_err(|_e| Error::I2C)
+        i2c.write(self.i2c_addr, &cmd).await.map_err(Error::I2C)
     }
 
-    async fn read(&self, i2c: &mut I2C, register: u16, buf: &mut [u8]) -> Result<(), Error> {
+    async fn read(&self, i2c: &mut I2C, register: u16, buf: &mut [u8]) -> Result<(), Error<E>> {
         i2c.write_read(self.i2c_addr, &register.to_be_bytes(), buf)
             .await
-            .map_err(|_e| Error::I2C)
+            .map_err(Error::I2C)
     }
 }
 
